b8c9600012a84f472389415393dd29a6c62a1432
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-01-30 14:09:47 +0100

    cleanup

--- godot-main-project/addons/living_platform_plugin/living_scene.gd
+++ godot-main-project/addons/living_platform_plugin/living_scene.gd
@@ -6,13 +6,3 @@ extends Node3D
 class_name LivingScene
 
 @export var OMEKA_BASE_URL: String = "https://omekas.livingculture.it"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
