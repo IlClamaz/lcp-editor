887d3ed7bb8899ea140006ba9b5c719ecac94331
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-02-05 18:03:45 +0100

    Added background panel to LivingImage class

--- godot-main-project/addons/living_platform_plugin/living_image.gd
+++ godot-main-project/addons/living_platform_plugin/living_image.gd
@@ -5,18 +5,35 @@ class_name LivingImage
 @export var image_path: String = "":
 	set(value):
 		image_path = value
-		_update_texture()
+		if is_node_ready():
+			_update_texture()
 
 @export var pixels_per_unit: float = 1.0:  # Optional: Adjust scale (e.g., 0.01 for smaller)
 	set(value):
 		pixels_per_unit = value
-		if has_node("."):  # Avoid init loop
+		if is_node_ready():
 			_update_texture()
 
+# This will contain the reference to the image 2D texture
 var current_texture: Texture2D
+# This will be added as child and will containg the box geometry acting as background
+var background: MeshInstance3D = null
+
+# The background thickness is computed as this factor of the video width
+const BACKGROUND_THICKNESS_PROP: float = 0.01
+# Absolute background padding size around the video area
+const BACKGROUND_PADDING: float = 0.2
+
 
 func _ready():
+	# Background rectangle (BoxMesh)
+	background = MeshInstance3D.new()
+	background.mesh = BoxMesh.new()
+	background.mesh.size = Vector3(1, 1, BACKGROUND_THICKNESS_PROP)  # Adjust as needed
+	add_child(background)
+	
 	_update_texture()
+
 
 func _update_texture():
 
@@ -35,9 +52,22 @@ func _update_texture():
 			material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 			material.cull_mode = BaseMaterial3D.CULL_DISABLED
 			material_override = material
+			
+			# Resizes the background
+			var background_w = quad_size.x + BACKGROUND_PADDING * 2
+			var background_h = quad_size.y + BACKGROUND_PADDING * 2
+			var background_depth = background_w * BACKGROUND_THICKNESS_PROP
+			background.mesh.size.x = background_w
+			background.mesh.size.y = background_h
+			background.mesh.size.z = background_depth
+			background.position.x = 0  # background_w / 2 - padding
+			background.position.y = 0
+			background.position.z = - 1.001 * background_depth / 2.0  # Behind video Sprite3D, with a additional epsilon to avoid z-fight
+		else:
+			push_error("Couldn't load imahe '%s'" % [image_path])
+			
 	else:
-		push_error("Image path '%s' doesn't exist " % image_path)
+		push_error("Image path '%s' doesn't exist " % [image_path])
 		current_texture = null
 		mesh = null
 		material_override = null
-		
