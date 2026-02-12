d26ff8618c7bf001ed83c4014ae56dd0d81201a1
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-02-12 13:47:28 +0100

    Added button and function to refresh all Elements of a scene.

--- godot-main-project/addons/living_platform_plugin/living_scene.gd
+++ godot-main-project/addons/living_platform_plugin/living_scene.gd
@@ -6,3 +6,25 @@ extends Node3D
 class_name LivingScene
 
 @export var OMEKA_BASE_URL: String = "https://omekas.livingculture.it"
+
+@export_tool_button("Refresh all Living Elements") var refresh_all_living_elements_btn = refresh_all_living_elements
+
+
+# Recurse the whole scene 
+func refresh_all_living_elements():
+	var scene_root: Node = null
+	if Engine.is_editor_hint():
+		scene_root = get_tree().edited_scene_root
+	else:
+		scene_root = get_tree().current_scene
+	
+	refresh_all_living_elements_R(scene_root)
+
+
+func refresh_all_living_elements_R(n: Node):
+	
+	if n is LivingElement:
+		n.fetch_omeka_info()
+	
+	for c in n.get_children():
+		refresh_all_living_elements_R(c)
