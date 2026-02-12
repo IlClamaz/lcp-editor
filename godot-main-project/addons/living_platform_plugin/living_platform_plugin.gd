92124de71cef8ef9ed152500fcf4e8aa74c67d0e
IlClamaz <vittorio.murtas@gmail.com>
2026-02-12 18:08:48 +0100

    XR and Curator Profile

--- godot-main-project/addons/living_platform_plugin/living_platform_plugin.gd
+++ godot-main-project/addons/living_platform_plugin/living_platform_plugin.gd
@@ -28,15 +28,15 @@ func _disable_plugin() -> void:
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type(LIVING_SCENE_CLASS_NAME, "Node3D", preload("living_scene.gd"), preload("LCLogo.png"))
-	add_custom_type(LIVING_AREA_CLASS_NAME, "Node", preload("living_area.gd"), preload("LCLogo.png"))
-	add_custom_type(LIVING_ITEM_CLASS_NAME, "Node", preload("living_item.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_MEDIA_CLASS_NAME, "Node3D", preload("living_media.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_IMAGE_CLASS_NAME, "MeshInstance3D", preload("living_image.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_TEXT_CLASS_NAME, "MeshInstance3D", preload("living_text.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_VIDEO_CLASS_NAME, "Sprite3D", preload("living_video.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_3DMODEL_CLASS_NAME, "Node3D", preload("living_3dmodel.gd"), preload("icon.svg"))
-	add_custom_type(LIVING_PORTAL_CLASS_NAME, "Node3D", preload("living_portal.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_SCENE_CLASS_NAME, "Node3D", preload("scripts/living_scene.gd"), preload("LCLogo.png"))
+	add_custom_type(LIVING_AREA_CLASS_NAME, "Node", preload("scripts/living_area.gd"), preload("LCLogo.png"))
+	add_custom_type(LIVING_ITEM_CLASS_NAME, "Node", preload("scripts/living_item.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_MEDIA_CLASS_NAME, "Node3D", preload("scripts/living_media.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_IMAGE_CLASS_NAME, "MeshInstance3D", preload("scripts/living_image.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_TEXT_CLASS_NAME, "MeshInstance3D", preload("scripts/living_text.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_VIDEO_CLASS_NAME, "Sprite3D", preload("scripts/living_video.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_3DMODEL_CLASS_NAME, "Node3D", preload("scripts/living_3dmodel.gd"), preload("icon.svg"))
+	add_custom_type(LIVING_PORTAL_CLASS_NAME, "Node3D", preload("scripts/living_portal.gd"), preload("icon.svg"))
 
 
 func _exit_tree() -> void:
---
+++ godot-main-project/data/info_entry.gd
@@ -0,0 +1,8 @@
+# res://data/info_entry.gd
+extends Resource
+class_name InfoEntry
+
+@export var title: String
+@export_multiline var preview_text: String
+@export_multiline var extended_text: String
+@export_multiline var interact_text: String
