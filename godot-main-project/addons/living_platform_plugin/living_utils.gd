25fadeaa0c3484a95b8cc929c3bd902a068a082a
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-02-13 11:38:50 +0100

    Added util to recursively get Node's AABB

---
+++ godot-main-project/addons/living_platform_plugin/living_utils.gd
@@ -0,0 +1,36 @@
+extends Node
+
+class_name LivingUtils
+
+static func set_owner_R(n: Node, owner: Node):
+	n.owner = owner
+	for c in n.get_children():
+		set_owner_R(c, owner)
+
+
+static func get_node_aabb(root: Node3D) -> AABB:
+	return _collect_aabb_recursive(root, root)
+
+
+static func _collect_aabb_recursive(root: Node3D, node: Node3D) -> AABB:
+	var result: AABB
+	var has_result := false
+
+	if node is VisualInstance3D:
+		var vi := node as VisualInstance3D
+		var local_aabb: AABB = vi.get_aabb()
+		var to_root: Transform3D = root.global_transform.affine_inverse() * node.global_transform
+		result = to_root * local_aabb
+		has_result = true
+
+	for child in node.get_children():
+		if child is Node3D:
+			var child_aabb := _collect_aabb_recursive(root, child)
+			if child_aabb: # if not null
+				if has_result:
+					result = result.merge(child_aabb)
+				else:
+					result = child_aabb
+					has_result = true
+
+	return result if has_result else null
