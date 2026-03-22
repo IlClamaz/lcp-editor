357f8a636fda3132168243081bb60f5fa362c399
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-03-22 20:41:49 +0100

    Long text caption is now created in front of the camera and animated to slide to the right of the observer.

--- godot-main-project/addons/living_platform_plugin/scripts/living_caption/caption_manager.gd
+++ godot-main-project/addons/living_platform_plugin/scripts/living_caption/caption_manager.gd
@@ -8,15 +8,17 @@ class_name CaptionManager
 
 @export_group("OFFSETS")
 ## Offset of the caption, with respect to the _camera, at the moment of visualization
-@export var caption_offset_pos: Vector3 = Vector3(2.5, 0, -3)
+@export var caption_offset_pos: Vector3 = Vector3(3.5, 0, -2)
 ## Y-rotation of the caption, with respect to the _camera, at the moment of visualization
-@export var caption_offset_y_rot: float = -30  # degrees
+@export var caption_offset_y_rot: float = -90.0  # degrees
 
 @export_group("")
 
 @export var font_color := Color(0.9, 0.9, 0.9)
 
-
+var _caption_starting_offset_pos: Vector3 = Vector3(0, 0, -3)
+var _caption_starting_scale: Vector3 = Vector3(0.1, 0.1, 0.1)
+var _caption_starting_offset_y_rot: float = 0.0
 
 ## The _camera controlling and updating this manager
 var _camera: LivingCamera = null
@@ -57,7 +59,7 @@ func create_description_object(living_element: LivingElement) -> void:
 	if living_element == _captioned_element:
 		return
 
-	# If another description was already visible, eli inate it.
+	# If another description was already visible, eliminate it.
 	if _is_caption_visible():
 		_destroy_description_object()
 
@@ -76,25 +78,55 @@ func create_description_object(living_element: LivingElement) -> void:
 	# Add the object to the scene at top level
 	_camera.get_tree().root.add_child(self._caption_obj)
 
+	# Set text and other properties
 	_caption_obj.set_text(text)
 	_caption_obj.set_text_color(font_color)
 
+	#
+	# Compute the global starting position and rotation according to the camera pos/rot
+	# Rotate the offset vector by the current _camera global rotation
+	var start_global_pos: Vector3 = _camera.global_position + (_camera.global_transform.basis) * _caption_starting_offset_pos
+	# Vertically align the description background to stay on front of the camera.
+	# Strong assumption that the floor is always at 0 height.
+	var description_aabb = LivingUtils.get_node_aabb(_caption_obj)
+	start_global_pos.y = start_global_pos.y + (description_aabb.size.y / 2.0)
+	# Compute the global y rotation
+	var start_global_y_rot = _camera.global_rotation_degrees.y + _caption_starting_offset_y_rot
+
+	_caption_obj.global_position = start_global_pos
+	_caption_obj.global_rotation_degrees = Vector3(0.0, start_global_y_rot, 0.0)
+	_caption_obj.scale = _caption_starting_scale
+
+	#
+	# Comput the global ending position and rotation
 	# Rotate the offset vector by the current _camera global rotation
 	var global_pos: Vector3 = _camera.global_position + (_camera.global_transform.basis) * caption_offset_pos
 	# Vertically align the description background to lay on the floor.
 	# Strong assumption that the floor is always at 0 height.
-	var description_aabb = LivingUtils.get_node_aabb(_caption_obj)
+	# var description_aabb = LivingUtils.get_node_aabb(_caption_obj)
 	global_pos.y = global_pos.y + (description_aabb.size.y / 2.0)
 	# Add the _camera y-rotation offset
 	var global_y_rot = _camera.global_rotation_degrees.y + caption_offset_y_rot
+
+	
 	
 	# print("CAM POS ", _camera.global_position, " ROT ", _camera.global_rotation)
 	# print("COMPUTED CAPTION POS ", global_pos, " Y-ROT ", global_y_rot)
 	#print("CAMERA GLOBAL ROT: ", _camera.global_rotation_degrees.y)
-	#print("SETTING: ", global_y_rot)
+	print("ROTATION FROM: ", start_global_y_rot, " --> " , global_y_rot)
 	
-	_caption_obj.global_position = global_pos
-	_caption_obj.global_rotation_degrees = Vector3(0.0, global_y_rot, 0.0)
+	# _caption_obj.global_position = global_pos
+	# _caption_obj.global_rotation_degrees = Vector3(0.0, global_y_rot, 0.0)
+
+	#
+	# Start the tweenings (all run in parallel)
+	var tween = _caption_obj.create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_parallel(true)
+	tween.tween_property(_caption_obj, "global_position", global_pos, 1.0)
+	tween.tween_property(_caption_obj, "scale", Vector3(1,1,1), 1.0)
+	tween.tween_property(_caption_obj, "global_rotation_degrees", Vector3(0, global_y_rot, 0), 1.0)
+
 	
 
 func _destroy_description_object() -> void:
