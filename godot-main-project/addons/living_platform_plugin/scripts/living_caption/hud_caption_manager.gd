ecc9416a3f21a34cb4a34175b2c976bf3ab0aee3
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-04-16 01:23:33 +0200

    Faster HUD appearance

--- godot-main-project/addons/living_platform_plugin/scripts/living_caption/hud_caption_manager.gd
+++ godot-main-project/addons/living_platform_plugin/scripts/living_caption/hud_caption_manager.gd
@@ -128,8 +128,8 @@ func _show_hud_3d_and_reveal() -> void:
 		# Start the tweening to move the HUD to the hud_offset position
 		#  and a second parallel tweening to scale the hud to the specified hud_scale
 		var tween := camera.create_tween().set_parallel(true)
-		tween.tween_property(_hud_text_3d, "position", hud_offset, 1.3)
-		tween.tween_property(_hud_text_3d, "scale", Vector3(hud_scale, hud_scale, hud_scale), 1.3)
+		tween.tween_property(_hud_text_3d, "position", hud_offset, 1.0)
+		tween.tween_property(_hud_text_3d, "scale", Vector3(hud_scale, hud_scale, hud_scale), 1.0)
 
 
 	var txt := _hud_closest_element.short_description
